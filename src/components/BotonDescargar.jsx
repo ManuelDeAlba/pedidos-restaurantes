9de9f8beb4bfb8a31755fa0355abc7941c86c629
Manuel De Alba <ferman5903@gmail.com>
2025-05-02 15:14:33 -0600

    Accept file name and detect file extension

--- src/components/BotonDescargar.jsx
+++ src/components/BotonDescargar.jsx
@@ -1,6 +1,6 @@
 import { cn } from "../lib/utils";
 
-function BotonDescargar({ datos, type="application/json", className, children, ...props }){
+function BotonDescargar({ datos, type="application/json", nombre="descarga", className, children, ...props }){
     const handleDescargarVentas = () => {
         // Si no hay datos, no se puede descargar nada
         if(!datos) toast.error("No hay datos para descargar");
@@ -12,7 +12,7 @@ function BotonDescargar({ datos, type="application/json", className, children, .
         // Crear un enlace para descargar el archivo
         const a = document.createElement("a");
         a.href = url;
-        a.download = `ventas-${new Date().toLocaleDateString().replaceAll("/", "-")}.json`;
+        a.download = `${nombre}-${new Date().toLocaleDateString().replaceAll("/", "-")}.${type.split("/")[1]}`; // Cambiar el nombre del archivo a ventas-YYYY-MM-DD.json
         a.click();
 
         // Borrar el objeto URL creado para liberar memoria
