e3ae0795770014270cf0231bec878733282813c4
Manuel De Alba <ferman5903@gmail.com>
2025-05-02 15:07:53 -0600

    Add download data button

---
+++ src/components/BotonDescargar.jsx
@@ -0,0 +1,31 @@
+import { cn } from "../lib/utils";
+
+function BotonDescargar({ datos, type="application/json", className, children, ...props }){
+    const handleDescargarVentas = () => {
+        // Si no hay datos, no se puede descargar nada
+        if(!datos) toast.error("No hay datos para descargar");
+
+        // Si hay datos, descargar en formato JSON
+        const blob = new Blob([JSON.stringify(datos)], { type });
+        const url = URL.createObjectURL(blob);
+
+        // Crear un enlace para descargar el archivo
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = `ventas-${new Date().toLocaleDateString().replaceAll("/", "-")}.json`;
+        a.click();
+
+        // Borrar el objeto URL creado para liberar memoria
+        URL.revokeObjectURL(url);
+    }
+
+    return(
+        <button
+            className={cn("block text-black/80 hover:text-black/90 underline font-semibold rounded-sm", className)}
+            onClick={handleDescargarVentas}
+            {...props}
+        >{children}</button>
+    )
+}
+
+export default BotonDescargar;
